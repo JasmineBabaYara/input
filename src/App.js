e0fd7b3cd098c23c5a3627fc04d9550535a915e1
JasmineBabaYara <jassyprincess1@gmail.com>
2021-08-12 23:12:34 -0700

    input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    this.state={
+      text:''
+    }
+    this.textChange =this.textChange.bind(this)
+  }
+  textChange(e){
+    this.setState({
+      [e.target.name]:e.target.value
+    })
+  }
+  render(){
+    return(
+      <div>
+        <input 
+          className='textinput'
+          type='text'
+          name='text'
+          value={this.state.text}
+          onChange={this.textChange}
+        />
+        <p>{this.state.text}</p>
+      </div>
+    )
+  }
 }
+
 
 export default App;
